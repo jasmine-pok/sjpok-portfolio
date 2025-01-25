84b5cce3be2b83fc7b9006f0f557594bc121eb10
jasmine-pok <socheatapok0801@gmail.com>
2025-01-24 23:29:08 -0600

    updated linking of hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,8 @@
-document.getElementById("hamburger-menu").addEventListener("click", function() {
-    const navList = document.getElementById("nav-list");
-    navList.classList.toggle("show"); // Toggle the visibility of the menu
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("hamburger-menu").addEventListener("click", function() {
+      const navList = document.getElementById("nav-list");
+      navList.classList.toggle("show"); // Toggle the visibility of the menu
   });
+});
+
   
