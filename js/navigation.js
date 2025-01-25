5757eeb7103dcfd5ec4b401439034b3379a68e23
jasmine-pok <socheatapok0801@gmail.com>
2025-01-24 23:09:06 -0600

    added hamburger menu for navbar

---
+++ js/navigation.js
@@ -0,0 +1,5 @@
+document.getElementById("hamburger-menu").addEventListener("click", function() {
+    const navList = document.getElementById("nav-list");
+    navList.classList.toggle("show"); // Toggle the visibility of the menu
+  });
+  
